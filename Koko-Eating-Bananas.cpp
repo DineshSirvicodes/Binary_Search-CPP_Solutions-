f18bd0eb998ddd65bdd0db25b487468c3bdb15ff
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2024-02-17 11:41:49 +0530

    Create Koko-Eating-Bananas.cpp

---
+++ Koko-Eating-Bananas.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long maxEl(vector<int> &piles){
+        int  maxi = INT_MIN;
+        int n = piles.size();
+        for(int i = 0; i<n ; i++){
+            maxi = max(maxi, piles[i]);
+        }
+        return maxi;
+    }
+    long long totalHoursFind(vector<int> &piles, int hourly){
+        long long n = piles.size();
+        long long totalHours = 0;
+        for(int i = 0 ; i<n ; i++){
+            totalHours += ceil((double)(piles[i])/(double)(hourly));
+        }
+        return totalHours;
+    }
+    long long minEatingSpeed(vector<int>& piles, int h) {
+
+        long long low = 1, high = maxEl(piles);
+        while(low<=high){
+            long long mid = (low+high)/2;
+            long long totalH = totalHoursFind(piles,mid);
+            if(totalH <= h){
+                                 //ans = mid;
+                high = mid-1;   // ele is possibe ans , but find min
+            }else{
+                low = mid+1;  // ele is too small 
+            }
+        }
+        return low;                        //return ans;
+    }
+};
